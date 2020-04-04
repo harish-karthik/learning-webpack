66b8915acb2ee24559da8a9dbe1060d64fe12df0
Harish Karthik <hhkk0010@outlook.com>
2020-04-04 17:35:05 +0530

    Created entry points for vendor and main js

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,10 @@ const path = require ('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry : './src/index.js',
+    entry : {
+        main :'./src/index.js',
+        vendor : './src/vendor.js'
+    },
     plugins: [new htmlWebpackPlugin({
         template : './src/template.html'
     })],
--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 module.exports = merge(common , {
     mode: 'production',
     output : {
-        filename : 'main.[ContentHash].js',
+        filename : '[name].[ContentHash].js',
         path : path.resolve(__dirname , "dist")
     },
     plugins : [new CleanWebpackPlugin()]
