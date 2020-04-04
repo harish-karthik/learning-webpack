a57323a53d0371afea1bbe7838ee287de0b66463
Harish Karthik <hhkk0010@outlook.com>
2020-04-04 17:21:30 +0530

    Added loaders and clean webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,18 @@ module.exports = {
             {
                 test : /\.scss$/,
                 use : ['style-loader','css-loader','sass-loader']
+            },
+            {
+                test : /\.html$/,
+                use : ['html-loader']
+            },
+            {
+                test : /\.(svg|png|jpg|gif)$/,
+                loader : 'file-loader',
+                options :{
+                    name: '[name].[ext]',
+                    outputPath : 'images'
+                }
             }
         ]
     }
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,13 @@ const path = require ('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const merge = require('webpack-merge')
 const common = require ('./webpack.common')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = merge(common , {
     mode: 'production',
     output : {
         filename : 'main.[ContentHash].js',
         path : path.resolve(__dirname , "dist")
-    }
+    },
+    plugins : [new CleanWebpackPlugin()]
 })
